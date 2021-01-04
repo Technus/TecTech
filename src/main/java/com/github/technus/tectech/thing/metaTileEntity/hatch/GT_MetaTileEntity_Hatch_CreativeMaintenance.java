d313167245a3123bafed94fe4b4c65e02b244fae
Prometheus0000000 <prometheus0000000@gmail.com>
2021-01-04 09:09:13 -0500

    Change name/description of the debug maint hatch

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/hatch/GT_MetaTileEntity_Hatch_CreativeMaintenance.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/hatch/GT_MetaTileEntity_Hatch_CreativeMaintenance.java
@@ -26,8 +26,8 @@ public class GT_MetaTileEntity_Hatch_CreativeMaintenance extends GT_MetaTileEnti
     public String[] getDescription() {
         return new String[]{
                 CommonValues.BASS_MARK,
-                translateToLocal("gt.blockmachines.debug.tt.maintenance.desc.0"),//For maintaining Multiblocks
-                translateToLocal("gt.blockmachines.debug.tt.maintenance.desc.1"),//Does fix everything but itself.
+                translateToLocal("gt.blockmachines.debug.tt.maintenance.desc.0"),//For automatically maintaining Multiblocks
+                translateToLocal("gt.blockmachines.debug.tt.maintenance.desc.1"),//Fixes everything including itself
                 EnumChatFormatting.AQUA + translateToLocal("gt.blockmachines.debug.tt.maintenance.desc.2")//Fixing is for plebs!
         };
     }
